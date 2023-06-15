3aa4861ed26eabe7b00ec797cb18f529ac25b7bb
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-06-15 14:01:09 +0200

    me quede implementando la lógica de si la pregunta es correcta

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ const nextInputSelector = document.querySelector("#next-input");
 const finishInputSelector = document.querySelector("#finish-input");
 const returnInputSelector = document.querySelector("#return-input");
 
+const newButtonSelector = document.querySelectorAll(".response");
+
 // Containers
 const questionContainerSelector = document.querySelector("#question-container");
 
@@ -36,45 +38,7 @@ let questionString = "";
 let correctAnswerString = "";
 let incorrectAnswersString = "";
 
-// Ocultar páginas
-const hidePages = () => {
-  homePageSelector.classList.add("hide");
-  questionPageSelector.classList.add("hide");
-  resultsPageSelector.classList.add("hide");
-  graphicsPageSelector.classList.add("hide");
-};
-
-// Funcionalidad
-// Escribimos en el dom la pregunta
-const writeQuestion = () => {
-  questionTextSelector.innerHTML = questions;
-};
-
-const writeAnswers = () => {};
-
-// Escribimos en el dom las respuestas con botones
-
-// Moverse entre páginas
-const goHomePage = () => {
-  hidePages();
-  homePageSelector.classList.remove("hide");
-};
-
-const goQuestionPage = () => {
-  hidePages();
-  writeQuestion();
-  questionPageSelector.classList.remove("hide");
-};
-
-const goResultsPage = () => {
-  hidePages();
-  resultsPageSelector.classList.remove("hide");
-};
-
-const goGraphicsPage = () => {
-  hidePages();
-  graphicsPageSelector.classList.remove("hide");
-};
+let indexQuestion = 0;
 
 // Api. En la api puedes seleccionar el número de pregutnas y su dificultad y tipo.
 // Obtenemos los datos de la api y los guardamos en variables globales
@@ -83,7 +47,7 @@ axios
   .then((res) => {
     questionsArray = res.data.results;
 
-    // console.log(res);
+    console.log(res);
 
     questionsArray.forEach((objectQuestions) => {
       questionString = objectQuestions.question;
@@ -97,6 +61,83 @@ axios
     });
   })
   .catch((err) => console.log(err));
+
+// Funcionalidad
+
+const nextQuestion = () => {};
+
+const checkAnswers = () => {
+  if (newButtonSelector.value == correctAnswerString) {
+    console.log("correcto");
+  } else {
+    console.log("incorrecto");
+  }
+};
+
+// Escribimos en el dom la pregunta y las respuestas
+const writeAnswers = () => {
+  totalAnswersArray.forEach((answer) => {
+    const newButton = document.createElement("button");
+    newButton.innerText = answer;
+    newButton.setAttribute("value", answer);
+    newButton.setAttribute("class", "response");
+
+    questionContainerSelector.appendChild(newButton);
+    checkAnswers();
+  });
+};
+
+const writeQuestion = () => {
+  questionTextSelector.innerHTML = questionString;
+  writeAnswers();
+};
+
+const startQuest = () => {
+  indexQuestion = 0;
+  startInputSelector.classList.add("hide");
+  writeQuestion();
+};
+
+// Guardamos en array las respuestas para tenerlas todas juntas
+const totalAnswers = () => {
+  incorrectAnswersArray.forEach((incorrectAnswer) => {
+    totalAnswersArray.push(incorrectAnswer);
+  });
+
+  totalAnswersArray.push(correctAnswerString);
+
+  startQuest();
+};
+
+// Ocultar páginas
+const hidePages = () => {
+  homePageSelector.classList.add("hide");
+  questionPageSelector.classList.add("hide");
+  resultsPageSelector.classList.add("hide");
+  graphicsPageSelector.classList.add("hide");
+};
+
+// Moverse entre páginas
+const goHomePage = () => {
+  hidePages();
+  homePageSelector.classList.remove("hide");
+};
+
+const goQuestionPage = () => {
+  hidePages();
+  totalAnswers();
+  questionPageSelector.classList.remove("hide");
+};
+
+const goResultsPage = () => {
+  hidePages();
+  resultsPageSelector.classList.remove("hide");
+};
+
+const goGraphicsPage = () => {
+  hidePages();
+  graphicsPageSelector.classList.remove("hide");
+};
 
 // Events click
 returnInputSelector.addEventListener("click", goHomePage);
